d7fbf74022e2dae25635e5c96edd4435c4e92a89
ahbeng <ah@beng.me>
2013-01-11 07:21:23 +0800

    Add some jobs using SQLAlchemy instead of SQL

--- initdb.py
+++ initdb.py
@@ -1,2 +1,16 @@
-from app.database import init_db
+from app.database import db_session, init_db
+
 init_db()
+
+from app.models import Job
+
+db_session.add_all([
+    Job('1000', 'http://www.ics.uci.edu/~bic/courses/JaverOS/pr3.pdf', 'http',
+        1, 'CS2106/pr3.pdf'),
+    Job('1001',
+        'http://ics.uci.edu/~bic/courses/OS-2012/Lectures-on-line/proj3.pptx',
+        'http', '1', 'CS2106/proj3.pptx'),
+    Job('1000', 'http://www.ics.uci.edu/~bic/courses/JaverOS/pr3.pdf', 'http',
+        '1', 'CS2106/hello.pdf')
+])
+db_session.commit()
