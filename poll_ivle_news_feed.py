636aeb146cfd910e79fcf51d17bfdac57b3204f9
Nicholas <nicholasluo@gmail.com>
2013-01-31 05:04:07 +0800

    Polling doesn't insert already existing entries (check against user id and announcement/file/forum id)

--- poll_ivle_news_feed.py
+++ poll_ivle_news_feed.py
@@ -4,6 +4,8 @@ from app.ivle import IvleClient
 from datetime import datetime
 from time import time
 
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
 def poll_news_feed(user_id):
     #get server values
     user = User.query.filter(User.user_id == user_id).one()
@@ -21,21 +23,38 @@ def poll_news_feed(user_id):
                 #print announcement["Creator"]["Name"]
                 #print datetime.fromtimestamp(int(announcement["CreatedDate"][6:16]))
                 #print announcement["Description"]
-                db_session.add(IVLEAnnouncement(announcement, courseCode, user.user_id))
-                db_session.commit()
+                try:
+                    IVLEAnnouncement.query\
+                        .filter(IVLEAnnouncement.user_id == user.user_id)\
+                        .filter(IVLEAnnouncement.ivle_id == announcement['ID'])\
+                        .one()
+                except MultipleResultsFound as e:
+                    pass
+                except NoResultFound as e:
+                    db_session.add(IVLEAnnouncement(announcement, courseCode, user.user_id))
+                    db_session.commit()
             forums = client.get('Forums', CourseID=result['ID'], Duration=0, IncludeThreads=True, TitleOnly=False)
-            print "---forum---"
+            #print "---forum---"
             for forum in forums['Results']:
                 #print forum.keys()
                 #print forum["Title"]
                 for heading in forum["Headings"]:
                     #print heading.keys()
                     #print heading["Title"]
-                    new_heading = IVLEForumHeading(forum, heading, courseCode, user.user_id)
-                    db_session.add(new_heading)
-                    db_session.commit()
+                    try:
+                        heading_db_entry = IVLEForumHeading.query\
+                            .filter(IVLEForumHeading.user_id == user.user_id)\
+                            .filter(IVLEForumHeading.ivle_heading_id == heading['ID'])\
+                            .one()
+                    except MultipleResultsFound as e:
+                        pass
+                    except NoResultFound as e:
+                        heading_db_entry = IVLEForumHeading(forum, heading, courseCode, user.user_id)
+                        db_session.add(heading_db_entry)
+                        db_session.commit()
+
                     #print heading
-                    exploreThreads(heading, user.user_id, new_heading.id, -1)
+                    exploreThreads(heading, user.user_id, heading_db_entry.id, -1)
                     #for thread in heading["Threads"]:
                     #    print thread.keys()
                     #    print thread["ID"]
@@ -49,21 +68,30 @@ def poll_news_feed(user_id):
                     #    print thread["Threads"]
             workbins = client.get('Workbins', CourseID=result['ID'], Duration=0)
             for workbin in workbins['Results']:
-                print workbin.keys()
+                #print workbin.keys()
                 title = workbin['Title']
                 exploreFolders(workbin, [courseCode + ' ' + title], workbin['ID'], client, user_id, courseCode)
-        print "------------"
+        #print "------------"
 
 
 def exploreThreads(json, user_id, parent_heading_id, parent_thread_id):
     for thread in json['Threads']:
-        print thread
-        print "----"
-        new_thread = IVLEForumThread(thread, user_id, parent_heading_id, parent_thread_id)
-        db_session.add(new_thread)
-        db_session.commit()
-        if not len(thread['Threads']) == 0:
-            exploreThreads(thread, user_id, parent_heading_id, new_thread.id)
+        #print thread
+        #print "----"
+        try:
+            thread_db_entry = IVLEForumThread.query\
+                                .filter(IVLEForumThread.user_id == user_id)\
+                                .filter(IVLEForumThread.ivle_id == thread['ID'])\
+                                .one()
+        except MultipleResultsFound as e:
+            pass
+        except NoResultFound as e:
+            thread_db_entry = IVLEForumThread(thread, user_id, parent_heading_id, parent_thread_id)
+            db_session.add(thread_db_entry)
+            db_session.commit()
+
+        if thread['Threads']:
+            exploreThreads(thread, user_id, parent_heading_id, thread_db_entry.id)
 
 
 def exploreFolders(json, parents, ivle_workbin_id, client, user_id, course_code):
@@ -71,18 +99,24 @@ def exploreFolders(json, parents, ivle_workbin_id, client, user_id, course_code)
     for folder in json['Folders']:
         folder_name = folder['FolderName']
         for file in folder['Files']:
-            file_name = file['FileName']
             file_id = file['ID']
             file_path = '/'.join(parents + [folder_name])
             file_url = client.build_download_url(file_id)
-            print file_path
-            print file
-            new_ivle_file = IVLEFile(file, file_path, user_id, ivle_workbin_id, course_code)
-            db_session.add(new_ivle_file)
-            db_session.commit()
-            #db_session.add(Job(fileID, fileURL, 'http', self.user.user_id,
-            #    filePath))
-            #db_session.commit()
+            #print file_path
+            #print file
+            try:
+                IVLEFile.query\
+                    .filter(IVLEFile.user_id == user_id)\
+                    .filter(IVLEFile.ivle_file_id == file_id)\
+                    .one()
+            except MultipleResultsFound as e:
+                pass
+            except NoResultFound as e:
+                new_ivle_file = IVLEFile(file, file_path, user_id, ivle_workbin_id, course_code)
+                db_session.add(new_ivle_file)
+                db_session.commit()
+                #db_session.add(Job(fileID, fileURL, 'http', self.user.user_id, filePath))
+                #db_session.commit()
         exploreFolders(folder, parents + [folder_name], ivle_workbin_id, client, user_id, course_code)
 
 
