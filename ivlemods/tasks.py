dfa540b2f9cf3bf71422a8b83f013d31d6ce9a9e
Nicholas <nicholasluo@gmail.com>
2013-03-05 17:43:41 +0800

    halt unstarted dropbox jobs if folder sync status changes

--- ivlemods/tasks.py
+++ ivlemods/tasks.py
@@ -37,6 +37,21 @@ def ivle_workbin_to_dropbox_job(user_id, duration=0):
                                        '/'.join([file.file_path, file.file_name])))
                     db_session.commit()
 
+@celery.task
+def halt_user_dropbox_jobs(user_id):
+    user = User.query.get(user_id)
+    folders = IVLEFolder.query.filter(IVLEFolder.user_id == user_id)\
+        .filter(IVLEFolder.sync == False)
+    client = IvleClient(user.ivle_token)
+    for folder in folders:
+        files = IVLEFile.query.filter(IVLEFile.user_id == user_id)\
+            .filter(IVLEFile.ivle_folder_id == folder.ivle_id)
+        for file in files:
+            jobs = Job.query.filter(Job.user_id == user_id)\
+            .filter(Job.file_id == file.ivle_file_id)
+            for job in jobs:
+                job.status = 6
+                db_session.commit()
 
 @celery.task
 def ivle_workbin_to_dropbox_jobs(duration=0):
