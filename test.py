d014054184718f7d376b9c23d707ee2c8b52e850
Nicholas <nicholasluo@gmail.com>
2013-03-07 01:34:06 +0800

    unit tests updated

--- test.py
+++ test.py
@@ -37,13 +37,14 @@ if User.query.filter(User.user_id == 1).count():
     time.sleep(20)
 
     test_folder = IVLEFolder.query.filter_by(user_id = 1).filter_by(ivle_id = 1).first()
-    print("1 Test for folder removal from ivle")
+    print("1 Test for folder delete")
     if test_folder.is_deleted == True:
         print("SUCCESS")
     else:
         print("FAILURE")
 
-    print("2 Test for file removal from ivle")
+    test_file = IVLEFile.query.filter_by(user_id = 1).filter_by(ivle_file_id = 10).first()
+    print("2 Test for file delete")
     if test_file.is_deleted == True:
         print("SUCCESS")
     else:
@@ -53,5 +54,39 @@ if User.query.filter(User.user_id == 1).count():
     db_session.delete(test_file)
     db_session.delete(test_folder)
     db_session.commit()
+
+    test_folder2 = IVLEFolder.query.filter_by(user_id = 1).filter_by(is_deleted = False).first()
+    test_folder2_id = test_folder2.ivle_id
+    test_folder2.is_deleted = True
+
+    test_file2 = IVLEFile.query.filter_by(user_id = 1).filter_by(is_deleted = False).first()
+    test_file2_id = test_file2.ivle_file_id
+    test_file2.is_deleted = True
+
+    db_session.commit()
+
+    result = poll_ivle_folders.poll_ivle_folders.delay(1)
+    #should wait for task to complete
+    time.sleep(20)
+
+    result_folder2 = IVLEFolder.query.filter_by(user_id = 1).filter_by(ivle_id = test_folder2_id)
+    print("3 Test for folder undelete")
+    print(result_folder2.first().ivle_id)
+    print(result_folder2.first().is_deleted)
+    if result_folder2.count() == 1 and result_folder2.first().is_deleted == False:
+        print("SUCCESS")
+    else:
+        print("FAILURE")
+
+    result_file2 = IVLEFile.query.filter_by(user_id = 1).filter_by(ivle_file_id = test_file2_id)
+    print("3 Test for folder undelete")
+    if result_file2.count() == 1 and result_file2.first().is_deleted == False:
+        print("SUCCESS")
+    else:
+        print("FAILURE")
+
+
+
+
 
 
