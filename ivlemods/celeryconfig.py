d7e1aca596499febbf5f60d2e1a33afd838cdbba
ahbeng <ah@beng.me>
2013-03-10 02:55:41 +0800

    Chain tasks into one_task_to_rule_them_all

--- ivlemods/celeryconfig.py
+++ ivlemods/celeryconfig.py
@@ -3,15 +3,11 @@ from __future__ import absolute_import
 from celery.schedules import crontab
 
 BROKER_URL = 'amqp://'
-CELERY_IMPORTS = ('ivlemods.dist', 'ivlemods.poll_ivle_folders', 'ivlemods.tasks')
+CELERY_IMPORTS = ['ivlemods.tasks']
 CELERY_TIMEZONE = 'Singapore'
 CELERYBEAT_SCHEDULE = {
-    'every-five-minutes': {
-        'task': 'ivlemods.dist.upload_dropbox_jobs',
-        'schedule': crontab(minute='*/1')
-        },
     'every-five-minutes-except-between-three-to-four-am': {
-        'task': 'ivlemods.tasks.ivle_workbin_to_dropbox_jobs',
+        'task': 'ivlemods.tasks.one_task_to_rule_them_all',
         'schedule': crontab(minute='*/5', hour='0-2,4-23'),
         'args': (5,),
         }
