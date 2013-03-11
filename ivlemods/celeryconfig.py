127a5b6813c2fd6f26f948c1ec22d145b95f8379
ahbeng <ah@beng.me>
2013-03-11 13:48:02 +0800

    Run beat tasks from 3-4am too

--- ivlemods/celeryconfig.py
+++ ivlemods/celeryconfig.py
@@ -8,6 +8,6 @@ CELERY_TIMEZONE = 'Singapore'
 CELERYBEAT_SCHEDULE = {
     'every-five-minutes-except-between-three-to-four-am': {
         'task': 'ivlemods.tasks.one_task_to_rule_them_all',
-        'schedule': crontab(minute='*/5', hour='0-2,4-23')
+        'schedule': crontab(minute='*/5')
         }
 }
