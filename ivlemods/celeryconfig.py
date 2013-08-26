d537d96893ffd1898d1e1de40e0f37e3d34d7a95
Nicholas Luo <nicholasluo@gmail.com>
2013-08-27 01:11:01 +0800

    no more need for beat

--- ivlemods/celeryconfig.py
+++ ivlemods/celeryconfig.py
@@ -6,12 +6,12 @@ BROKER_URL = 'redis://localhost:6379/0'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_IMPORTS = ['ivlemods.tasks']
 CELERY_TIMEZONE = 'Singapore'
-CELERYBEAT_SCHEDULE = {
-    'every-five-minutes-except-between-three-to-four-am': {
-        'task': 'ivlemods.tasks.one_task_for_them_all',
-        'schedule': crontab(minute='*/5')
-    }
-}
+#CELERYBEAT_SCHEDULE = {
+#    'every-five-minutes-except-between-three-to-four-am': {
+#        'task': 'ivlemods.tasks.one_task_for_them_all',
+#        'schedule': crontab(minute='*/5')
+#    }
+#}
 CELERY_ROUTES = {
     'ivlemods.poll_ivle_folders.poll_ivle_folders': {'queue': 'ivle'},
     'ivlemods.poll_ivle_modules.poll_ivle_modules': {'queue': 'ivle'},
