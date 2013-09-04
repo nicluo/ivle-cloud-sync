dd64710ff48f9430e8f3fdd5ed9c98f72dc0330f
Nicholas Luo <nicholasluo@gmail.com>
2013-09-04 17:07:09 +0800

    improvements to monitor

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -8,8 +8,8 @@ file_handler.setFormatter(logging.Formatter(
 ))
 file_handler.setLevel(logging.DEBUG)
 
-mail_handler = SMTPHandler('smtp.gmail.com', 'ivlecloudsync@gmail.com',
-    'nicholasluo@gmail.com', 'IVLE Cloud Sync Error',
+mail_handler = SMTPHandler(('smtp.gmail.com', 587), 'ivlecloudsync@gmail.com',
+    ['nicholasluo@gmail.com'], 'CloudSync Monitor Error',
     credentials=('ivlecloudsync@gmail.com', '***REMOVED***'), secure=())
 mail_handler.setFormatter(logging.Formatter('''
 Message type:       %(levelname)s
@@ -22,7 +22,7 @@ Message:
 
 %(message)s
 '''))
-mail_handler.setLevel(logging.ERROR)
+mail_handler.setLevel(logging.WARNING)
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
@@ -30,3 +30,23 @@ root.setLevel(logging.DEBUG)
 main_logger = logging.getLogger('monitor')
 main_logger.addHandler(file_handler)
 main_logger.addHandler(mail_handler)
+
+schedule_mail_handler = SMTPHandler(('smtp.gmail.com', 587), 'ivlecloudsync@gmail.com',
+    ['nicholasluo@gmail.com'], 'CloudSync Monitor Scheduled Message',
+    credentials=('ivlecloudsync@gmail.com', '***REMOVED***'), secure=())
+mail_handler.setFormatter(logging.Formatter('''
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Module:             %(module)s
+Function:           %(funcName)s
+Time:               %(asctime)s
+
+Message:
+
+%(message)s
+'''))
+schedule_mail_handler.setLevel(logging.INFO)
+
+mail_logger = logging.getLogger('mail')
+mail_logger.addHandler(schedule_mail_handler)
+mail_logger.addHandler(file_handler)
--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,32 +1,135 @@
 from __future__ import absolute_import
-import datetime
-import time
+from datetime import datetime, timedelta, date, time
+from time import sleep
 import logging
 import numpy as np
 logger = logging.getLogger(__name__)
+mail_logger = logging.getLogger('mail.monitor')
 
 from ivlemods.celery import celery
 
+def gen_six_hourly():
+    #generate 6 hourly dates
+    dates = [datetime.combine(date.today(), time(0)),
+             datetime.combine(date.today(), time(6)),
+             datetime.combine(date.today(), time(16, 30)),
+             datetime.combine(date.today(), time(18))]
+    return dates
+
+def cycle(dates):
+    #cycles the dates so that dates[0] will be closest forward to current time
+    #dates that are passed will be removed, added a day, and appended to the queue
+    while(dates[0] < datetime.now()):
+        dt = dates[0] 
+        dates.pop(0)
+        dates.append(dt + timedelta(days=1))
+    return
+
+def check_dates(dates):
+    #checks if the time coincides with the listed
+    #automatically cycles the dates
+    if(dates[0] < datetime.now()):
+        cycle(dates)
+        return True
+    else:
+        return False
+
+def create_alarm(minutes, alarm_list):
+    #appends alarm to alarm_list
+    alarm_list.append(minutes)
+    alarm_list.sort(reverse=True)
+
+def check_alarms(inactive_minutes, alarm_list):
+    #triggers mail logging
+    for times in alarm_list:
+        if(inactive_minutes > times):
+            return times
+    return False
+
+def email_statistics(times):
+    #consolidates and emails statistics
+    run_times = np.array(times)
+    average_time = timedelta(seconds = np.average(run_times))
+    max_time = timedelta(seconds = int(np.amax(run_times)))
+    min_time = timedelta(seconds = int(np.amin(run_times)))
+    total = len(times)
+    mail_logger.info('one_task has been run a total of %s times. Average: %s, Max: %s, Min: %s.', total, average_time, max_time, min_time)
+
 def watch():
-    state = 0
-    now = datetime.datetime.now()
+    #main loop to be called
+    #dev
+    #inspect_worker_name = 'celery1.localhost'
+    inspect_worker_name = 'celery.cloudsync'
+
+    #generate the times when statistics are mailed
+    mail_times = gen_six_hourly()
+    cycle(mail_times)
+
+    #generate the alarm times for problems to be raised
+    alarm_list = []
+    create_alarm(15, alarm_list)
+    create_alarm(30, alarm_list)
+    create_alarm(60, alarm_list)
+
+    #initial states
+    times = []
+    #state = 1 to ignore first data point
+    state = 1
+    now = datetime.now()
+    #alarm state to propagate
+    task_alarm_state = 0
+    worker_alarm_state = 0
+
     while True:
-        i = celery.control.inspect(['celery1.localhost'])
-        #print len(i.active()['celery1.localhost'])
-        scheduled = len(i.scheduled()['celery1.localhost'])
-        times = []
+        #check for alarms
+        if(state == 0):
+            diff = datetime.now() - now
+            res = check_alarms(diff.total_seconds() / 60, alarm_list)
+            if res and res > alarm_state:
+                #ignore repeat alarms from the same limit
+                alarm_state = res
+                #alarmed
+                logger.warning('Alarm triggered. CloudSync one_task hasn\'t been scheduled for %s minutes.', alarm_state) 
+        else:
+            #reset alarm
+            alarm_state = 0
+
+        #collect statistics
+        i = celery.control.inspect([inspect_worker_name])
+        scheduled = 0
+        if i.scheduled():
+            scheduled = len(i.scheduled()[inspect_worker_name])
+            worker_alarm_state = 0
+        elif worker_alarm_state == 0:
+            #worker isn't alive
+            logger.warning('Worker %s not inspectable. Is the worker running and alive.', inspect_worker_name) 
+            #ignore repeat alarms
+            worker_alarm_state = 1
+
+
+        #check for scheduled reports
+        if check_dates(mail_times):
+            #mail results here
+            if(len(times)):
+                email_statistics(times)
+            else:
+                logger.info('Not enough run statistics. Not reporting statistics')
+            times = []
+
         if not state == scheduled:
+
+            #state is the number of things in the scheduled queue
+            logger.debug(state)
             state = scheduled
-            print('-----')
-            logger.info(state)
-            diff = datetime.datetime.now() - now
+            diff = datetime.now() - now
+            now = datetime.now()
+
+            #logging
             if state == 0 :
-                logger.debug('Scheduled task detected')
+                logger.info('No scheduled tasks - one_task should be running')
             elif state == 1 :
                 times.append(int(diff.total_seconds()))
-                logger.debug('No scheduled tasks')
-                logger.info('Average')
-                logger.info(datetime.timedelta(seconds = np.average(np.array(times))))
-            logger.info(now)
-            now = datetime.datetime.now()
-        time.sleep(10)
+                logger.info('Scheduled task detected - one_task is queued')
+                logger.info('Current Run %s', diff)
+
+        sleep(10)
